66b122441b6cbea4253ea6fcd266f31d8245daed
haanmiba <haanmiba@gmail.com>
2018-12-26 01:16:09 -0500

    Added channel info functionality

--- main.js
+++ main.js
@@ -63,6 +63,11 @@ function handleSignoutClick(event) {
   gapi.auth2.getAuthInstance().signOut();
 }
 
+function showChannelData(data) {
+  const channelData = document.getElementById("channel-data");
+  channelData.innerHTML = data;
+}
+
 // Get channel from API
 function getChannel(channel) {
   gapi.client.youtube.channels
@@ -71,7 +76,28 @@ function getChannel(channel) {
       forUsername: channel
     })
     .then(response => {
-      console.log(response);
+      const channel = response.result.items[0];
+      const output = `
+        <ul class="collection">
+          <li class="collection-item">Title: ${channel.snippet.title}</li>
+          <li class="collection-item">ID: ${channel.id}</li>
+          <li class="collection-item">Subscribers: ${
+            channel.statistics.subscriberCount
+          }</li>
+          <li class="collection-item">Views: ${
+            channel.statistics.viewCount
+          }</li>
+          <li class="collection-item">Videos: ${
+            channel.statistics.videoCount
+          }</li>
+        </ul>
+        <p>${channel.snippet.description}</p>
+        <hr>
+        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+          channel.snippet.customUrl
+        }">Visit Channel</a>
+        `;
+      showChannelData(output);
     })
     .catch(err => alert("No channel by that name."));
 }
