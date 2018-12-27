1c1a5bf7d061dece6f9eba65ef837b96a0b8ea18
haanmiba <haanmiba@gmail.com>
2018-12-27 01:37:39 -0500

    Debugging result

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ function getSubscriptions() {
       })
       .then(response => {
         console.log(response);
-        console.log(repsonse.result.nextPageToken);
+        console.log(repsonse.result);
         nextPageToken = response.result.nextPageToken;
       })
       .catch(err => alert("There was an issue fetching your subscriptions."));
