b662e4c44f237119928153279e64afdff212cc61
haanmiba <haanmiba@gmail.com>
2019-01-03 21:21:06 -0500

    Fixed bug with skip button and round number

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ function clearTimer(event = undefined) {
 
 function switchPhase(event) {
   if (currentPhase === "focus") {
-    if (event !== undefined) {
+    if (event === undefined) {
       roundNumber++;
     }
     updatePhaseNumber();
