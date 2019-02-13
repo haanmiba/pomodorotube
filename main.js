f1b3f350a1cb9e1eb48c798442be8e95e8513a4f
Hans Bas <mr.hans.bas@gmail.com>
2019-02-13 00:00:35 -0500

    Testing reverting from switch case

--- main.js
+++ main.js
@@ -448,22 +448,18 @@ function getVideoLength(video) {
   let timeMatches = video.contentDetails.duration.match(digitsRegex);
   timeMatches = timeMatches.map(match => parseInt(match));
   let numSeconds = 0;
-  switch (timeMatches.length) {
-    case 3:
-      const [hours, minutes, seconds] = timeMatches;
-      numSeconds += hours * Math.pow(SECONDS_IN_A_MINUTE, 2);
-      numSeconds += minutes * SECONDS_IN_A_MINUTE;
-      numSeconds += seconds;
-      break;
-    case 2:
-      const [minutes, seconds] = timeMatches;
-      numSeconds += minutes * SECONDS_IN_A_MINUTE;
-      numSeconds += seconds;
-      break;
-    case 1:
-      const [seconds] = timeMatches;
-      numSeconds += seconds;
-      break;
+  if (timeMatches.length === 3) {
+    const [hours, minutes, seconds] = timeMatches;
+    numSeconds += hours * Math.pow(SECONDS_IN_A_MINUTE, 2);
+    numSeconds += minutes * SECONDS_IN_A_MINUTE;
+    numSeconds += seconds;
+  } else if (timeMatches.length === 2) {
+    const [minutes, seconds] = timeMatches;
+    numSeconds += minutes * SECONDS_IN_A_MINUTE;
+    numSeconds += seconds;
+  } else if (timeMatches.length === 1) {
+    const [seconds] = timeMatches;
+    numSeconds += seconds;
   }
   return numSeconds;
 }
